cd4b3c760c09cc5357f0a34bee6e7d3f4d32d52d
Filippo <filippo.vallarino@gmail.com>
2022-09-19 12:18:18 +0200

    aggiunto esercizio 2

---
+++ esercizio2.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+let nome_utente = prompt("inserisci nome ");
+let eta_utente = prompt("inserisci età ");
+
+let numero_cabina = Math.floor(Math.random() * 25);
+
+if(eta_utente<18){
+    console.log("non puoi ancora votare!")
+}else{
+    console.log("il tuo nome è " + nome_utente + " e la tua età è " + eta_utente + ". La tua cabina per votare è la numero " + numero_cabina);
+};
